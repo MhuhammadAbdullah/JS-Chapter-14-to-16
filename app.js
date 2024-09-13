7970e6ff285462da902f877ebb4d13b7a8e64a18
MhuhammadAbdullah <abaloch0033@gmail.com>
2024-09-13 11:47:18 +0500

    updated

---
+++ app.js
@@ -0,0 +1,29 @@
+// Assignment: 05
+// Question: 01
+
+// var studentNames = [];
+// console.log(studentNames);
+
+// Question: 02
+
+// var studentData = {names: []};
+  
+// Question: 03
+
+// var stringArray = ["Alice", "Bob", "Charlie"];
+// console.log(stringArray);
+// Question: 04
+
+// var numberArray = [10, 20, 30, 40, 50];
+// console.log(numberArray);
+
+// Question: 05
+
+// var booleanArray = [true, false, true, false];
+// console.log(booleanArray);
+
+// Question: 06
+
+// Mixed array with different types of values
+// var mixedArray = ["Alice", 30, true, null, { name: "Bob" }, [1, 2, 3]];
+// console.log(mixedArray);
